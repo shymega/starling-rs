0ac47c8ef7dbf948b88921d184fcd5a11ae95d35
Dom Rodriguez <shymega@shymega.org.uk>
2021-03-19 16:12:12 +0000

    Populate Transaction model
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/models/common/transaction.rs
+++ src/models/common/transaction.rs
@@ -1,9 +1,30 @@
+use crate::models::common::CurrencyAndAmount;
 use uuid::Uuid as UUID;
 
-#[derive(Clone, Debug, Copy)]
+type DateTimeISO = String;
+
 #[derive(Clone, Debug, Default)]
+#[allow(non_snake_case)]
 pub struct Transaction {
     pub feed_item_uid: UUID,
     pub category_uid: UUID,
-}
+    pub amount: CurrencyAndAmount,
+    pub sourceAmount: CurrencyAndAmount,
+    pub direction: String,
+    pub updatedAt: DateTimeISO,
+    pub transactionTime: DateTimeISO,
+    pub settlementTime: DateTimeISO,
+    pub source: String,
+    pub status: String,
+    pub counterPartytype: String,
+    pub counterPartyUid: UUID,
+    pub counterPartyName: String,
+    pub counterPartySubEntityUid: UUID,
+    pub counterPartySubEntityName: String,
+    pub counterPartySubEntityIdentifier: String,
+    pub counterPartySubEntitySubIdentifier: String,
+    pub reference: String,
+    pub country: String,
+    pub spendingCategory: String,
+    pub hasAttachment: bool,
 }
