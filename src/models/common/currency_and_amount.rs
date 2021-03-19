e39dd70f77ae06182ac465f00380b8fb805c65bf
Dom Rodriguez <shymega@shymega.org.uk>
2021-03-19 16:03:51 +0000

    Apply fix to minorUnit property on core CurrencyAndAmount struct
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/models/common/currency_and_amount.rs
+++ src/models/common/currency_and_amount.rs
@@ -1,14 +1,14 @@
 use std::string::ToString;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Default, Debug)]
 pub struct CurrencyAndAmount {
-    pub currency: String,
-    pub minor_units: i64,
+    currency: String,
+    minor_units: i32,
 }
 
 impl CurrencyAndAmount {
     #[allow(clippy::clippy::must_use_candidate)]
-    pub fn new(currency: &str, minor_units: i64) -> CurrencyAndAmount {
+    pub fn new(currency: &str, minor_units: i32) -> CurrencyAndAmount {
         CurrencyAndAmount {
             currency: currency.to_owned(),
             minor_units,
@@ -16,25 +16,25 @@ impl CurrencyAndAmount {
     }
 
     #[allow(clippy::clippy::must_use_candidate)]
+    pub fn get_currency(&self) -> String {
+        self.currency.clone()
+    }
+
+    #[allow(clippy::clippy::must_use_candidate)]
+    pub fn get_minor_units(&self) -> i32 {
+        self.minor_units
+    }
+
+    #[allow(clippy::clippy::must_use_candidate)]
     #[allow(clippy::cast_precision_loss)]
     pub fn to_conventional_units(&self) -> f64 {
-        (self.minor_units / 100) as f64
+        f64::from(self.minor_units) / f64::from(100)
     }
 }
 
 impl ToString for CurrencyAndAmount {
     fn to_string(&self) -> String {
         format!("{} {}", self.to_conventional_units(), self.currency)
-    }
-}
-
-impl Default for CurrencyAndAmount {
-    #[allow(clippy::clippy::must_use_candidate)]
-    fn default() -> CurrencyAndAmount {
-        CurrencyAndAmount {
-            currency: "XXX".to_owned(),
-            minor_units: 0,
-        }
     }
 }
 
--- src/models/common/transaction.rs
+++ src/models/common/transaction.rs
@@ -1,16 +1,9 @@
 use uuid::Uuid as UUID;
 
 #[derive(Clone, Debug, Copy)]
+#[derive(Clone, Debug, Default)]
 pub struct Transaction {
     pub feed_item_uid: UUID,
     pub category_uid: UUID,
 }
-
-impl Default for Transaction {
-    fn default() -> Transaction {
-        Transaction {
-            feed_item_uid: UUID::new_v4(),
-            category_uid: UUID::new_v4(),
-        }
-    }
 }
