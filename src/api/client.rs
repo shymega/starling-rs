869c4baa967e706266780231488d62504c0e1033
Dom Rodriguez <shymega@shymega.org.uk>
2021-03-19 16:19:44 +0000

    Remove custom Debug implementation for now
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,5 +1,4 @@
 use reqwest::Client as reqwest_client;
-use std::fmt;
 use crate::models::enums::Endpoint;
 
 #[allow(dead_code)]
@@ -7,14 +6,4 @@ use crate::models::enums::Endpoint;
 pub struct Client {
     http: reqwest_client,
     endpoint: Endpoint,
-}
-
-impl fmt::Debug for Client {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Credentials: {:?}\nClient: not available.",
-            self.auth
-        )
-    }
 }
