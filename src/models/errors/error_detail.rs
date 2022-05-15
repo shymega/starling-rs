2b884b116b5db07ad4a9ddc25280a0342943508b
Dom Rodriguez <shymega@shymega.org.uk>
2022-05-15 22:53:10 +0100

    Add `new` fn to ErrorDetail model
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/models/errors/error_detail.rs
+++ src/models/errors/error_detail.rs
@@ -2,3 +2,12 @@
 pub struct ErrorDetail {
     pub message: String,
 }
+
+impl ErrorDetail {
+    pub fn new(msg: &str) -> Self {
+        Self {
+            message: String::from(msg)
+        }
+    }
+}
+
